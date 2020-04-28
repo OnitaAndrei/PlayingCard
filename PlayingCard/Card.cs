0d5068714a4702205cf73ed2d212b9d5eeaeaa74
gogo fresh <tigerkevin379@gmail.com>
2020-04-28 19:04:40 +0300

    Add project files.

---
+++ PlayingCard/Card.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Playingcarte
+{
+    internal class carte
+    {
+        int nr, culoare;
+        public static int As= 1,Doiar=2,Treiar=3,Patrar=4,Cinci=5,Sase=6,Septar=7,Opt=8,Noua=9,Zeaca=10,Juvete=11,Dama=12,Popa=13,Carou=1,Trefla=2,InimaR=3,InimaN=4;
+        bool isnr(int nr)
+        { 
+            if (As <= nr && nr <= Popa)
+                return true;
+            return false;
+         }   
+
+        bool isculoare(int culoare)
+        {
+            if(Carou <= culoare && culoare <= InimaN)
+                return true;
+            return false;
+        }
+
+        public carte(int nr, int culoare)
+        {
+            bool isnrBool = isnr(nr), isculoareBool = isculoare(culoare); 
+            if (!isnrBool)
+                throw new ArgumentException("Numar invalid");
+            if (!isculoareBool)
+                throw new ArgumentException("Culoare invalida");
+            this.nr = nr;
+            this.culoare = culoare;
+        }
+
+        public int getnr() { return nr; }
+        public int getculoare() { return culoare; }
+        public static String nrToString(int nr)
+        {
+            switch (nr)
+            {
+                case 1:
+                    return "As";
+                case 2:
+                    return "Doi";
+                case 3:
+                    return "Trei";
+                case 4:
+                    return "Patru";
+                case 5:
+                    return "Cinci";
+                case 6:
+                    return "Sase";
+                case 7:
+                    return "Sapte";
+                case 8:
+                    return "Opt";
+                case 9:
+                    return "Noua";
+                case 10:
+                    return "Zece";
+                case 11:
+                    return "Juvete";              
+                case 12:
+                    return "Dama";
+                case 13:
+                    return "Popa";
+                default:
+                    return null;
+            }
+        }
+        public static String culoareToString(int culoare)
+        {
+            switch(culoare)
+            {
+                case 1:
+                    return "de Carou";
+                case 2:
+                    return "de Trefla";
+                case 3:
+                    return "de Inima Rosie";
+                case 4:
+                    return "de Inima Neagra";
+                default:
+                    return null;
+
+            };
+        }
+    }
+}
---
+++ PlayingCard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Playingcarte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                while (true)
+                {
+
+                    Random rnd = new Random();
+                    carte carte = new carte(rnd.Next(1,14), rnd.Next(1,5));
+                    int nrcarte = carte.getnr();
+                    int culoarecarte = carte.getculoare();
+                    Console.WriteLine("Numarul cartii este {0}.", nrcarte);
+                    Console.WriteLine("Numarul culorii cartii este {0}.", culoarecarte);
+                    Console.WriteLine("Cartea este {0} {1}.", carte.nrToString(nrcarte), carte.culoareToString(culoarecarte));
+                    Console.ReadKey();
+                }           
+        }
+    }
+}
